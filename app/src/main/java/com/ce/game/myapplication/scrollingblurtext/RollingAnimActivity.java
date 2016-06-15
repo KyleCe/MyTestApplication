45b373688a0ca9937919badad2c02892e81e1753
kylece <kcstep@126.com>
2016-06-15 19:15:27 +0800

    add floating guide view with repeating animation, the animation is attached to different views, and can be switch down if necessary

--- app/src/main/java/com/ce/game/myapplication/scrollingblurtext/RollingAnimActivity.java
+++ app/src/main/java/com/ce/game/myapplication/scrollingblurtext/RollingAnimActivity.java
@@ -2,22 +2,13 @@ package com.ce.game.myapplication.scrollingblurtext;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.TranslateAnimation;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.ce.game.myapplication.R;
+import com.ce.game.myapplication.scrollingblurtext.userguideanim.AssistGuideView;
+import com.ce.game.myapplication.scrollingblurtext.userguideanim.FloatViewModel;
 import com.ce.game.myapplication.util.DU;
-import com.ce.game.myapplication.util.ViewU;
-
-import java.util.concurrent.CountDownLatch;
 
 public class RollingAnimActivity extends AppCompatActivity {
 
@@ -43,8 +34,6 @@ public class RollingAnimActivity extends AppCompatActivity {
             "Attention Fuel", "King Locker Ann", "Judging machine", "King of fighter", "Tencent wechat", "Sina Weibo", "AZ Recorder", "Smooth Shooter", "Smart Cleaner Pro", "Zombie park",
     };
 
-    ImageView mVerticalHand;
-    ImageView mHorizontalHand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +42,6 @@ public class RollingAnimActivity extends AppCompatActivity {
         Context context = RollingAnimActivity.this;
 
         listView = (ListView) findViewById(R.id.list_view);
-        mVerticalHand = (ImageView) findViewById(R.id.hand);
-        mHorizontalHand = (ImageView) findViewById(R.id.horizontal_hand);
 
         MyCustomAdapter customAdapter = new MyCustomAdapter(context);
         customAdapter.addData(appNames);
@@ -64,166 +51,30 @@ public class RollingAnimActivity extends AppCompatActivity {
         listView.post(new Runnable() {
             @Override
             public void run() {
-                if (mOnlySecondScene) {
-                    horizontalHanAnim(mHorizontalHand);
-                    ViewU.hide(mVerticalHand);
-                    return;
-                }
-
-                scroll(SCROLLING_DURATION);
-                verticalHandAnim(mVerticalHand);
-                mFirstDoneLatch = new CountDownLatch(firstRepeatTimes);
-                mSecondDoneLatch = new CountDownLatch(firstRepeatTimes);
-                mFirstSceneTask = new FirstSceneTask();
-                mSecondSceneTask = new SecondSceneTask();
-
-                DU.execute(mFirstSceneTask);
-                DU.execute(mSecondSceneTask);
             }
         });
 
-    }
 
-    FirstSceneTask mFirstSceneTask;
-    SecondSceneTask mSecondSceneTask;
-    private int firstRepeatTimes = 3;
+        final FloatViewModel mAssistGuideModel = new FloatViewModel(this);
 
-    private static final int HIDE_FIRST_SCENE = 70;
-    private static final int START_FIRST_SCENE = 231;
-    private static final int START_SECOND_SCENE = 930;
-    private static final int ONLY_SECOND_SCENE = 396;
-    boolean mOnlySecondScene = true;
-
-    Handler mHandler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case HIDE_FIRST_SCENE:
-                    break;
-                case START_FIRST_SCENE:
-                    mSecondDoneLatch = new CountDownLatch(firstRepeatTimes);
-                    mHorizontalHand.clearAnimation();
-                    ViewU.invisible(mHorizontalHand);
-                    verticalHandAnim(mVerticalHand);
-                    DU.execute(mSecondSceneTask);
-                    break;
-                case START_SECOND_SCENE:
-                    mFirstDoneLatch = new CountDownLatch(firstRepeatTimes);
-                    mVerticalHand.clearAnimation();
-                    ViewU.hide(mVerticalHand);
-                    horizontalHanAnim(mHorizontalHand);
-                    DU.execute(mFirstSceneTask);
-                    break;
-                case ONLY_SECOND_SCENE:
-
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-
-    private class FirstSceneTask implements Runnable {
-
-        @Override
-        public void run() {
-            DU.sd("second", "run" + DU.time());
-            try {
-
-                mFirstDoneLatch.await();
-
-                mHandler.sendEmptyMessage(START_SECOND_SCENE);
-                DU.sd("second", "reset");
-            } catch (InterruptedException e) {
-                DU.sd("second", "interrupted" + e + DU.time());
-            }
-        }
-    }
-
-    private class SecondSceneTask implements Runnable {
-
-        @Override
-        public void run() {
-            DU.sd("second", "run" + DU.time());
-            try {
-
-                mSecondDoneLatch.await();
-
-                mHandler.sendEmptyMessage(START_FIRST_SCENE);
-                DU.sd("second", "reset");
-            } catch (InterruptedException e) {
-                DU.sd("second", "interrupted" + e + DU.time());
-            }
-        }
-    }
-
-    CountDownLatch mFirstDoneLatch;
-    CountDownLatch mSecondDoneLatch;
-
-    private void verticalHandAnim(View view) {
-        TranslateAnimation mRollingAnim = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0,
-                Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF,
-                0, Animation.RELATIVE_TO_PARENT, 0.5f);
-
-        mRollingAnim.setAnimationListener(new Animation.AnimationListener() {
+        AssistGuideView guideView = new AssistGuideView(RollingAnimActivity.this);
+        guideView.attachCallBack(new AssistGuideView.GuideCallback() {
             @Override
-            public void onAnimationStart(Animation animation) {
-                DU.sd("listener", "start");
-//                if(mFirstDoneLatch.getCount() == firstRepeatTimes -1)
-//                    ViewU.show(mVerticalHand);
+            public void onTouch() {
             }
 
             @Override
-            public void onAnimationEnd(Animation animation) {
-                DU.sd("listener", "end");
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                DU.sd("listener", "repeat");
-                if (mFirstDoneLatch != null) mFirstDoneLatch.countDown();
+            public void onButtonClick() {
+                if (null == mAssistGuideModel) return;
+                mAssistGuideModel.clearView();
             }
         });
-        mRollingAnim.setRepeatCount(Animation.INFINITE);
-        mRollingAnim.setRepeatMode(Animation.RESTART);
-        mRollingAnim.setInterpolator(new LinearInterpolator());
-        mRollingAnim.setDuration(1000);
-        view.startAnimation(mRollingAnim);
+//        guideView.displaySecondSceneOnly();
+        guideView.startAnimation();
+
+        mAssistGuideModel.setView(guideView, 0);
     }
 
-    private void horizontalHanAnim(View v) {
-
-        TranslateAnimation mRollingAnim = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0,
-                Animation.RELATIVE_TO_PARENT, 0.5f, Animation.RELATIVE_TO_SELF,
-                0, Animation.RELATIVE_TO_PARENT, 0);
-
-        mRollingAnim.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                DU.sd("h listener", "start");
-//                if(mFirstDoneLatch.getCount() == firstRepeatTimes -1)
-//                    ViewU.show(mVerticalHand);
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                DU.sd("h listener", "end");
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                DU.sd("h listener", "repeat");
-                if (mSecondDoneLatch != null) mSecondDoneLatch.countDown();
-            }
-        });
-        mRollingAnim.setRepeatCount(Animation.INFINITE);
-        mRollingAnim.setRepeatMode(Animation.RESTART);
-        mRollingAnim.setInterpolator(new LinearInterpolator());
-        mRollingAnim.setDuration(1000);
-        v.startAnimation(mRollingAnim);
-
-    }
 
     private void scroll(int DURATION) {
         listView.smoothScrollBy(0, 0); // Stops the listview from overshooting.
--- app/src/main/java/com/ce/game/myapplication/scrollingblurtext/userguideanim/AssistGuideView.java
+++ app/src/main/java/com/ce/game/myapplication/scrollingblurtext/userguideanim/AssistGuideView.java
@@ -1,22 +1,57 @@
 package com.ce.game.myapplication.scrollingblurtext.userguideanim;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.Transformation;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ce.game.myapplication.R;
+import com.ce.game.myapplication.util.DU;
+import com.ce.game.myapplication.util.ViewU;
+
+import java.util.concurrent.CountDownLatch;
 
 public class AssistGuideView extends FrameLayout {
     private TextView tvGuideUser;
 
+    private GuideCallback mCallback;
+
+    private LinearLayout mSecondSceneParent;
+    private ProgressBar mSlideProgress;
+
+    private ImageView mVerticalHand;
+    private ImageView mHorizontalHand;
+    private CountDownLatch mFirstDoneLatch;
+    private CountDownLatch mSecondDoneLatch;
+
+    private FirstSceneTask mFirstSceneTask;
+    private SecondSceneTask mSecondSceneTask;
+    private int mRepeatTimes = 2;
+
+    private static final int START_FIRST_SCENE = 231;
+    private static final int START_SECOND_SCENE = 930;
+    private boolean bSecondSceneOnly = false;
+
     public interface GuideCallback {
         void onTouch();
+
+        void onButtonClick();
     }
+
     public AssistGuideView(final Context context) {
         this(context, null);
     }
@@ -29,7 +64,25 @@ public class AssistGuideView extends FrameLayout {
         super(context, attrs, defStyleAttr);
         inflate(context, R.layout.assist_guide_view, this);
 
+        init(context);
+    }
+
+    private void init(Context ctx) {
         tvGuideUser = (TextView) findViewById(R.id.tv_guide_user);
+
+        mSecondSceneParent = (LinearLayout) findViewById(R.id.second_scene_parent);
+
+        mVerticalHand = (ImageView) findViewById(R.id.vertical_hand);
+        mHorizontalHand = (ImageView) findViewById(R.id.horizontal_hand);
+
+        mSlideProgress = (ProgressBar) findViewById(R.id.open_process_indicator);
+
+        findViewById(R.id.stop_btn).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCallback.onButtonClick();
+            }
+        });
     }
 
     /**
@@ -55,12 +108,7 @@ public class AssistGuideView extends FrameLayout {
         if (callback == null)
             throw new NullPointerException("call back cannot be null");
 
-//        this.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                callback.onTouch();
-//            }
-//        });
+        mCallback = callback;
 
         setOnTouchListener(new OnTouchListener() {
             @Override
@@ -69,6 +117,157 @@ public class AssistGuideView extends FrameLayout {
                 return false;
             }
         });
+    }
+
+    public void displaySecondSceneOnly() {
+        bSecondSceneOnly = true;
+    }
+
+    public void startAnimation() {
+        if (bSecondSceneOnly) {
+            horizontalHanAnim(mHorizontalHand);
+            ViewU.hide(mVerticalHand);
+            return;
+        }
+        ViewU.hideAndShow(mSecondSceneParent, mVerticalHand);
+
+        verticalHandAnim(mVerticalHand);
+        mFirstDoneLatch = new CountDownLatch(mRepeatTimes);
+        mSecondDoneLatch = new CountDownLatch(mRepeatTimes);
+        mFirstSceneTask = new FirstSceneTask();
+        mSecondSceneTask = new SecondSceneTask();
+
+        DU.execute(mFirstSceneTask);
+        DU.execute(mSecondSceneTask);
+    }
+
+    Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case START_FIRST_SCENE:
+                    mSecondDoneLatch = new CountDownLatch(mRepeatTimes);
+                    mHorizontalHand.clearAnimation();
+                    ViewU.hideAndShow(mSecondSceneParent, mVerticalHand);
+                    verticalHandAnim(mVerticalHand);
+                    DU.execute(mSecondSceneTask);
+                    break;
+                case START_SECOND_SCENE:
+                    mFirstDoneLatch = new CountDownLatch(mRepeatTimes);
+                    mVerticalHand.clearAnimation();
+                    ViewU.hideAndShow(mVerticalHand, mSecondSceneParent);
+                    horizontalHanAnim(mHorizontalHand);
+                    DU.execute(mFirstSceneTask);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    private class FirstSceneTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                mFirstDoneLatch.await();
+                mHandler.sendEmptyMessage(START_SECOND_SCENE);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    private class SecondSceneTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                mSecondDoneLatch.await();
+                mHandler.sendEmptyMessage(START_FIRST_SCENE);
+            } catch (InterruptedException e) {
+                DU.sd("second", "interrupted" + e + DU.time());
+            }
+        }
+    }
+
+    private void verticalHandAnim(View view) {
+        TranslateAnimation mRollingAnim = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_PARENT, 0.2f,
+                Animation.RELATIVE_TO_SELF, 0);
+
+        mRollingAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                if (mFirstDoneLatch != null) mFirstDoneLatch.countDown();
+            }
+        });
+        mRollingAnim.setRepeatCount(Animation.INFINITE);
+        mRollingAnim.setRepeatMode(Animation.RESTART);
+        mRollingAnim.setInterpolator(new LinearInterpolator());
+        mRollingAnim.setDuration(1500);
+        view.startAnimation(mRollingAnim);
+    }
+
+    private void horizontalHanAnim(View v) {
+
+        TranslateAnimation mRollingAnim = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, 0.8f, Animation.RELATIVE_TO_SELF,
+                0, Animation.RELATIVE_TO_PARENT, 0);
+
+        mRollingAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                if (mSecondDoneLatch != null) mSecondDoneLatch.countDown();
+            }
+        });
+        mRollingAnim.setRepeatCount(Animation.INFINITE);
+        mRollingAnim.setRepeatMode(Animation.RESTART);
+        mRollingAnim.setInterpolator(new LinearInterpolator());
+        mRollingAnim.setDuration(1200);
+        v.startAnimation(mRollingAnim);
+
+        ProgressBarAnimation anim = new ProgressBarAnimation(mSlideProgress, 0f, 100f);
+        anim.setDuration(1200);
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setRepeatMode(Animation.RESTART);
+        mSlideProgress.startAnimation(anim);
+    }
+
+    final private class ProgressBarAnimation extends Animation {
+        private ProgressBar progressBar;
+        private float from;
+        private float to;
+
+        public ProgressBarAnimation(ProgressBar progressBar, float from, float to) {
+            super();
+            this.progressBar = progressBar;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            super.applyTransformation(interpolatedTime, t);
+            float value = from + (to - from) * interpolatedTime;
+            progressBar.setProgress((int) value);
+        }
+
     }
 
     @Override
