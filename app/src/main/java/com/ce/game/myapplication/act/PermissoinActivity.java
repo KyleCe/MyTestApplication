834c66aa7def672059c2197cda7bc93767b7a4cc
kylece <kcstep@126.com>
2016-08-05 10:37:39 +0800

    lock screen first launch animation test

---
+++ app/src/main/java/com/ce/game/myapplication/act/PermissoinActivity.java
@@ -0,0 +1,15 @@
+package com.ce.game.myapplication.act;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ce.game.myapplication.R;
+
+public class PermissoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissoin);
+    }
+}
