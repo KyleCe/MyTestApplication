d8f496d6b70d688458b35777e16075c3609e0a8c
kylece <kcstep@126.com>
2016-08-30 16:02:22 +0800

    gif activity test

---
+++ app/src/main/java/com/ce/game/myapplication/util/Const.java
@@ -0,0 +1,17 @@
+package com.ce.game.myapplication.util;
+
+/**
+ * Created by KyleCe on 2016/8/30.
+ *
+ * @author: KyleCe
+ */
+
+public class Const {
+
+
+    public static final String KEY = "gif_key";
+    public static final int TYPE_ONE = 1;
+    public static final int TYPE_TWO = 2;
+    public static final int TYPE_THREE = 3;
+    public static final int TYPE_FOUR = 4;
+}
